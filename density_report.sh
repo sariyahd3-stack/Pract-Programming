cfadf13fd2bdf71c941fbc695d0ff1c05d730304
Sariyah <sariyahd3@gmail.com>
2025-11-07 01:11:44 +0000

    FINSIHED density_report.sh for HW2

--- density_report.sh
+++ density_report.sh
@@ -4,7 +4,7 @@
 FILE="/home/shared/FA25_coding/Exam_1/SLF_genomic.gff"
 
 #echo the header "Chromosome   GeneCount   Class"
-echo -e "Chromosome\tGeneCount\tClass"
+echo -e "Chromosome\tGeneCount\tClass" > density_report.txt #create a file called density_report.txt with the header in it 
 
 chromosome=($(grep -v "#" "$FILE" | cut -f1 | sort |uniq)) #the variable chromosome equals the chromosomes names from the input file sorted uniquely 
 
@@ -15,19 +15,21 @@ for chr in "${chromosome[@]}" ; do #for the value of every chromosome name in th
 
 	#echo -e "${chr}\t${count}" #echo both the chromosome name and its gene count
 
-	if [ $count -gt 2000 ] ; then #if the number of genes is greater than 2000 #if the gne count it greater than 2000 then
+		if [ $count -gt 2000 ] ; then #if the number of genes is greater than 2000 #if the gne count it greater than 2000 then
 
-                class="High-Density" #classify the value  as high density #using the variable class classify it equal to High-Density 
+                	class="High-Density" #classify the value  as high density #using the variable class classify it equal to High-Density 
 
-        else #if its not greater than 2000
+        	else #if its not greater than 2000
 
-                class="Low-Density" #classify the value  as low density
+                	class="Low-Density" #classify the value  as low density
 
-        fi #close if/else loop
+        	fi #close if/else loop
        
-	 echo -e "${chr}\t${count}\t${class}" #echo the list into columns of the chromosome names, its gene count, and whether its high or low density
+	 echo -e "${chr}\t${count}\t${class}" >> density_report.txt  #echo the list into columns of the chromosome names, its gene count, and whether its high or low density and append it to the density_report.txt file 
 
 done #close for loop 
+
+echo "Completed density_report.txt" #print the completion of the for loop into the density_report.txt file 
 
 #end of script 
 
